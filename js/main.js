f266870e542b44767ec9e162964007cc8dac5a7c
Gustavo N <gustavostnt@gmail.com>
2018-08-22 23:07:06 -0300

    some js for fun

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 
-console.log("hello world");
+function S2B_FB_LOGIN() {
+
+    const initial = document.getElementById("initial");
+    const signed = document.getElementById("signed");
+    const error = document.getElementById("error");
+
+    const login = document.getElementById("login");
+    const logout = document.getElementById("logout");
+    const trial = document.getElementById("try");
+
+    const loginHandler = () => {
+        initial.classList.toggle("hide")
+        signed.classList.toggle("hide")
+    };
+    
+    const logoutHandler = () => {
+        initial.classList.toggle("hide")
+        signed.classList.toggle("hide")
+    };
+    
+    login.addEventListener("click", loginHandler);
+    logout.addEventListener("click", logoutHandler);
+
+}
+
+S2B_FB_LOGIN();

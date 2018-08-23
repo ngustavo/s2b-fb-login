f471094c372b900aab8ea691bda24faf15ac7773
Gustavo N <gustavostnt@gmail.com>
2018-08-23 00:15:45 -0300

    trying to login with facebook

--- js/main.js
+++ js/main.js
@@ -10,13 +10,28 @@ function S2B_FB_LOGIN() {
     const trial = document.getElementById("try");
 
     const loginHandler = () => {
-        initial.classList.toggle("hide")
-        signed.classList.toggle("hide")
+
+        const appid = "2034262413570505";
+        const redir = "https://www.facebook.com/connect/login_success.html";
+        const red = "http://localhost:5000";
+
+        const compose = `https://www.facebook.com/v3.1/dialog/oauth?
+            client_id=${appid}&display=popup&redirect_uri=${redir}
+            &onlogin="document.location.href=document.location.href;"`;
+
+        window.open(compose);
+
+        // axios.get(, { 
+        //         headers: {'Access-Control-Allow-Origin': '*'}
+        //     });
+
+        initial.classList.toggle("hide");
+        signed.classList.toggle("hide");
     };
     
     const logoutHandler = () => {
-        initial.classList.toggle("hide")
-        signed.classList.toggle("hide")
+        initial.classList.toggle("hide");
+        signed.classList.toggle("hide");
     };
     
     login.addEventListener("click", loginHandler);
